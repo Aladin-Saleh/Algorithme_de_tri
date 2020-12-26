f42a3ed00fea57bb8800bf29904805143aaed5cd
saleh <aladi@MSI.localdomain>
2020-12-26 06:09:46 +0100

    début  formule math

--- js/test.js
+++ js/test.js
@@ -17,4 +17,41 @@ console.log(Object.keys(votes.saleh));
 console.log(Object.values(votes.saleh.ACDA));
 
 
+
+
+
+//Formule mathématique pour faire le classement
+//Brouillon 
+var user1;
+var user2;
+var user3;
+var user4;
+//Chaque user possède un tableau contenant les users qui on voté pour lui
+
+var usersPoint = [user1 =[user1,user3,user4,user2],user2 = [user1,user4],user3 = [user1,user2],user4 = [user3]];//Tableau qui contient les tableaux de vote de tout les users
+//user1 = 3        user2 = 2           user3 = 2         user4 = 1
+var tableauDePoint = [];//Initialisation du tableau de point
+var tableauClassement = [];//Initialisation du tableau de classement
+
+for (let index = 0; index < usersPoint.length; index++) {
+    tableauDePoint[index] = usersPoint[index].length;//On met dans le tableau de point le nombre de votes reçu par chaque user dans l'ordre 1-4
+    var id = index +1;//Sert seulement pour l'affichage
+    console.log("User"+id+" : "+tableauDePoint[index]);//Debugage des valeurs
+    for (let index2 = 0; index2 < tableauDePoint.length; index2++) {
+        var buf = tableauDePoint[index];
+        
+        
+    }
+
+}
+
+for (let index = 0; index < usersPoint.length; index++) {
+    console.log(index + " --+ " + usersPoint[index].includes(user1));//Test : Est supposé vérifier si ça contient un user
+
+    
+    
+}
+
+
+
  

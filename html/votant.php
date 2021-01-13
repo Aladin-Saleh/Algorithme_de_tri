30224f9c756f4b48d6c05f27b457e8eaff043bc0
minguetp <paull.minguet@gmail.com>
2021-01-13 16:20:29 +0100

    beau tableau

--- html/votant.php
+++ html/votant.php
@@ -1,6 +1,6 @@
 <?php
 
-echo count(getListeVotant())."<br>";
+//echo count(getListeVotant())."<br>";
 /*
  * 1. Créer le graphe p° et la matrice P grâce aux votes.
  * p° = 1/nombre de votants
@@ -37,10 +37,7 @@ echo count(getListeVotant())."<br>";
     private $nom;
    
     private $poids = 1/143;
-    //private $score = array(1/141 , 1/141, 1/141, 1/141, 1/141, 1/141, 1/141, 1/141, 1/141, 1/141);
-    //private $score = array(10,10,10,10,10,10,10,10,10,10);
     private $score = array(1/143 * 1/143,1/143 * 1/143,1/143 * 1/143,1/143 * 1/143,1/143 * 1/143,1/143 * 1/143,1/143 * 1/143,1/143 * 1/143,1/143 * 1/143,1/143 * 1/143);
-    //private $score = array(0,0,0,0,0,0,0,0,0,0);
     public function __construct($n)
     {
       $this->nom = $n;
@@ -48,20 +45,11 @@ echo count(getListeVotant())."<br>";
 
     }
 
-    function upgrade_score($matiere,$nombre_de_vote_emis) // 0 = acda, 1 = anglais, 2 = apl, 4 = art, 4 = asr, 5 = ec, 6 = egod, 7 = maths, 8 = sgbd, 9 = sport
+    function upgrade_score($matiere,$nombre_de_vote_emis) 
     {
-      //$this->score[$matiere] += $this->score[$matiere] *(1/$nombre_de_vote_emis);
-      $this->score[$matiere] += $this->score[$matiere] *(1/$nombre_de_vote_emis);
-      //$this->score[$matiere] +=  $this->poids * $nombre_de_vote_emis;
+      $this->score[$matiere] += 0.15/143 + 0.85*(1/143);
     }
 
-  /*  function diluer_score($nombre_de_vote_emis) {
-      $alpha = 0.15;
-      //echo "diluage";
-      for($i = 0; $i < 10; $i++){
-        $this->score[$i] = ((1-$alpha) * $this->score[$i] + (($alpha) * 1/$nombre_de_vote_emis));
-      }
-    } */
     function diluer_score() {
       $alpha = 0.15;
       //echo "diluage";
@@ -121,121 +109,75 @@ echo count(getListeVotant())."<br>";
     $tab = array();
     $tab = getListeVotant();
     $matiere = array("ACDA","ANG","APL","ART","ASR","EC","EGOD","MAT","SGBD","SPORT");
-
     $obj = json_decode(file_get_contents('../JSON/www.iut-fbleau.fr.json'));
 
-     
-
-      for ($i=0; $i < count($tab); $i++) { 
+    for ($i=0; $i < count($tab); $i++) { 
         $nom_eleve = $tab[$i];
         $votant_score_obj[$i] = new VotantData($nom_eleve);
         for ($j=0; $j < count($tab); $j++) { 
           for($l = 0; $l < 10; $l++){
             $mat = $matiere[$l];
             $votant_sc = $tab[$j]; 
-            
-              
               if (isset($obj->$votant_sc->$mat)) 
               {
-                
                 for ($k=0; $k <count($obj->$votant_sc->$mat) ; $k++) { 
                   if ($nom_eleve == $obj->$votant_sc->$mat[$k]) {
-                  
                     $votant_score_obj[$i]->upgrade_score($l,count($obj->$votant_sc->$mat));
-                   
                   }
-                  //echo "<br>";
                 }
               }
-              
             
           }
         }
-       
-
       }
-
       for ($i=0; $i <count($tab) ; $i++) { 
         $votant_score_obj[$i]->diluer_score();
       }
 
+      for ($i=0; $i < count($tab); $i++) { 
 
-    
-
-    
-
-
-    for ($i=0; $i < count($tab); $i++) { 
-
-      $matiere = array("ACDA","ANG","APL","ART","ASR","EC","EGOD","MAT","SGBD","SPORT");
-      $nom_eleve = $tab[$i];
-
-      echo $nom_eleve;
-      echo "<br>";
-      for($j = 0; $j < 10; $j++) {
-        echo $matiere[$j];
-        echo " : ";
-        echo $votant_score_obj[$i]->get_score($j);
-        echo "<br>";
+        $matiere = array("ACDA","ANG","APL","ART","ASR","EC","EGOD","MAT","SGBD","SPORT");
+        $nom_eleve = $tab[$i];
+  
+        //echo $nom_eleve;
+        //echo "<br>";
+        for($j = 0; $j < 10; $j++) {
+          //echo $matiere[$j];
+          //echo " : ";
+          //echo $votant_score_obj[$i]->get_score($j);
+          //echo "<br>";
+        }
+        //echo "------------------------------------------------<br>";
       }
-      echo "------------------------------------------------<br>";
-    }
+
+    
+
+    
+
+
     return $votant_score_obj;
   }
 
   getScore();
 
-  function getScore()
-  {
-
-    if(isset($_POST["math"]) || isset($_POST["anglais"]) || isset($_POST["EC"]) || isset($_POST["EGOD"]) || isset($_POST["ASR"]) || isset($_POST["ACDA"]) || isset($_POST["SGBD"]) || isset($_POST["APL"]) || isset($_POST["ART"]) || isset($_POST["SPORT"])) 
-    {
+  function getScore() {
+    if(isset($_POST["math"]) || isset($_POST["anglais"]) || isset($_POST["EC"]) || isset($_POST["EGOD"]) || isset($_POST["ASR"]) || isset($_POST["ACDA"]) || isset($_POST["SGBD"]) || isset($_POST["APL"]) || isset($_POST["ART"]) || isset($_POST["SPORT"])) {
       $nom_eleve;
+      $tab_matieres = array();
       $tab = array();
       $tab = getListeVotant();
       $top_score = array();
       $matiere = array("ACDA","ANG","APL","ART","ASR","EC","EGOD","MAT","SGBD","SPORT");
+      $coche = array("ACDA", "anglais", "APL", "ART", "ASR", "EC", "EGOD", "math", "SGBD", 'SPORT');
       $votant_score_obj = setPoint();
-      if(isset($_POST["ACDA"])) {
-        echo "acda a été coché<br>";
-        prems(0, $votant_score_obj);
+
+      for($i = 0; $i < count($coche); $i++) {
+        if(isset($_POST[$coche[$i]])) {
+          //echo "$coche[$i] a été coché<br>";
+          array_push($tab_matieres, $i);
+        }
       }
-      if(isset($_POST["anglais"])) {
-        echo "anglais a été coché<br>";
-        prems(1, $votant_score_obj);
-      }
-      if(isset($_POST["APL"])) {
-        echo "apl a été coché<br>";
-        prems(2, $votant_score_obj);
-      }
-      if(isset($_POST["ART"])) {
-        echo "art a été coché<br>";
-        prems(3, $votant_score_obj);
-      }
-      if(isset($_POST["ASR"])) {
-        echo "asr a été coché<br>";
-        prems(4, $votant_score_obj);
-      }
-      if(isset($_POST["EC"])) {
-        echo "ec a été coché<br>";
-        prems(5, $votant_score_obj);
-      }
-      if(isset($_POST["EGOD"])) {
-        echo "egod a été coché<br>";
-        prems(6, $votant_score_obj);
-      }
-      if(isset($_POST["math"])) {
-        echo "maths a été coché<br>";
-        prems(7, $votant_score_obj);
-      }
-      if(isset($_POST["SGBD"])) {
-        echo "sgbd a été coché<br>";
-        prems(8, $votant_score_obj);
-      }
-      if(isset($_POST["SPORT"])) {
-        echo "sport a été coché<br>";
-        prems(9, $votant_score_obj);
-      }
+      prems($tab_matieres, $votant_score_obj);
     }
   }
 
@@ -243,24 +185,48 @@ echo count(getListeVotant())."<br>";
     $nom_eleve;
     $tab = array();
     $tab = getListeVotant();
-    $premier_score = $votant_score_obj[0]->get_score($choix);
+    $score_eleves = array();
+    $premier_score = 0;
     $premier_nom;
     $matiere = array("ACDA","ANG","APL","ART","ASR","EC","EGOD","MAT","SGBD","SPORT");
     for ($i=0; $i < count($tab); $i++) {
+      $score = 0;
       $nom_eleve = $tab[$i];
-      if ($votant_score_obj[$i]->get_score($choix) > $premier_score ) {
-        $premier_score = $votant_score_obj[$i]->get_score($choix);
+      for($j = 0; $j < count($choix); $j++){
+        $score += $votant_score_obj[$i]->get_score($choix[$j]);
+      }
+      $score = $score/count($choix);
+      $score_eleves[$nom_eleve] = $score;
+      arsort($score_eleves);
+      if ($score > $premier_score ) {
+        $premier_score = $score;
         $premier_nom = $nom_eleve;
       }
     }
 
-    echo "Le plus fort c'est lui ! <br>";
+    /*echo "<br>Le plus fort c'est lui ! <br>";
     echo $premier_nom;
     echo " : ";
-    echo $matiere[$choix];
+    for($i = 0; $i < count($choix); $i++) {
+      echo $matiere[$choix[$i]];
+      if(count($choix) > 1)
+        echo ", ";
+    }
     echo " = ";
     echo $premier_score;
-    echo "<br><br>";
+    echo "<br><br><br>";
+    echo "Les autres : <br>";*/
+    for($i = 0; $i < count($choix); $i++) {
+      echo $matiere[$choix[$i]];
+      if(count($choix) > 1)
+        echo ", ";
+    }
+    echo "<table>";
+    $count = 1;
+    foreach ($score_eleves as $nom_eleve => $score) {
+      echo "<tr><th>$count</th><th>$nom_eleve</th><th>$score</th></tr>";
+      $count++;
+    }
   }
 
 ?>
